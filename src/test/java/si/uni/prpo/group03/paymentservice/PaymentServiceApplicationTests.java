a2ce360af88c00c8fb489981c6a7e9ab62643412
jakepel03 <jaka.pelko03@gmail.com>
2024-10-21 15:34:14 +0200

    Initial commit

---
+++ src/test/java/si/uni/prpo/group03/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package si.uni.prpo.group03.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
