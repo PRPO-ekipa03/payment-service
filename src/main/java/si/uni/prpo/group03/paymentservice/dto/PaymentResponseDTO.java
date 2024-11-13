071c4203aa9db6a0701012d4165e7360fbcec14f
Ruben Ferreira <rf5885@student.uni-lj.si>
2024-11-13 18:36:39 +0100

    Initial DTOs structure

---
+++ src/main/java/si/uni/prpo/group03/paymentservice/dto/PaymentResponseDTO.java
@@ -0,0 +1,33 @@
+package si.uni.prpo.group03.paymentservice.dto;
+
+public class PaymentResponseDTO {
+
+    private String approvalUrl;
+    private String orderId;
+    private String status;
+
+    // Getters and Setters
+    public String getApprovalUrl() {
+        return approvalUrl;
+    }
+
+    public void setApprovalUrl(String approvalUrl) {
+        this.approvalUrl = approvalUrl;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
